f3c9ada424e22ad3b8a76c3920ddffb9ed2e1b48
GMD1ALajos <lajosdegraaf@hotmail.com>
2016-10-24 13:00:41 +0200

    24-10-2016 flipperkast layout
    Layout van flipperkast gemaakt.
    Mappen structuur gemaakt

---
+++ Project Flipperkast/Assets/Scripts/ForcePush.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePush : MonoBehaviour {
+
+    public Vector3 direction;
+    public Rigidbody ball;
+    public int force;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        direction = collision.contacts[0].point;
+        ball.AddForce(direction * force);
+    }
+    public void Update() {
+
+
+    }
+}
