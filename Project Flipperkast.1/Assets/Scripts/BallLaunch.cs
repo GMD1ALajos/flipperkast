c95d624ab66368ecaba8de3a7d7ead94bde9845b
GMD1ALajos <lajosdegraaf@hotmail.com>
2016-11-03 09:31:19 +0100

    Pinball update 3-11-2016
    Models & Layout

---
+++ Project Flipperkast.1/Assets/Scripts/BallLaunch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLaunch : MonoBehaviour {
+
+    public float downtimer;
+    public bool buttonpress;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            downtimer = 0;
+            downtimer = downtimer + 100 * Time.deltaTime;
+            buttonpress = true;
+        }
+
+        else
+
+        {
+            buttonpress = false;
+        }
+        if (downtimer >= 200)
+        {
+            downtimer = 200;
+        }
+
+    }
+}
