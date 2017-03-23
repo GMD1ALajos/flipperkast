8b13c3c5aa6685480d15cb316939e89d48e5ad5b
Lajos de Graaf <lajosdegraaf@hotmail.com>
2017-03-23 14:54:54 +0100

    Added a lot.

---
+++ Project 3/MyBeeGame/Assets/Scripts/Player/SCR_PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed;
+
+	void Start ()
+    {
+        Cursor.visible = false;
+	}
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Cursor.visible = true;
+        }
+    }
+	
+	void FixedUpdate ()
+    {
+        float moveH = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        transform.Translate(moveH, 0, 0);
+	}
+}
